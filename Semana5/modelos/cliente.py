246af26d3a11a0b2dda51126ea22557564c4f0f0
Jostin194 <js.villaltac@uea.edu.ec>
2026-06-28 15:04:57 -0500

    Delete Semana5 directory

--- Semana5/modelos/cliente.py
+++
@@ -1,8 +0,0 @@
-class Cliente:
-    def __init__(self,nombre: str, celular : int):
-        #Cliente guarda su nombre y numero
-        self.nombre: str = nombre
-        self.celular: int = celular
-
-    def __str__(self)-> str:
-        return f"Cliente: {self.nombre} , celular: {self.celular}"
